4a31710614b2ea9473372e4267d57bba85122281
ChristianEbnoether <30667511+ChristianEbnoether@users.noreply.github.com>
2022-06-28 23:40:58 +0200

    Update watering.py

--- watering.py
+++ watering.py
@@ -19,21 +19,12 @@ relais4_gpio = 5 #Pump
 relais3_gpio = 4 #Ring3
 relais2_gpio = 3 #Ring2
 relais1_gpio = 2 #Ring1
-dev_path = '/sys/bus/w1/devices/'
-dev_Dach = glob.glob(dev_path + '28-0316611d8bff')[0]
-dev_Vorlauf = glob.glob(dev_path + '28-031660882fff')[0]
-dev_Nachlauf = glob.glob(dev_path + '28-04166127baff')[0]
-dev_Luft = glob.glob(dev_path + '28-031660c2f9ff')[0]
-Luft = dev_Luft + '/w1_slave'
-Dach = dev_Dach + '/w1_slave'
-Vorlauf = dev_Vorlauf + '/w1_slave'
-Nachlauf = dev_Nachlauf + '/w1_slave'
 host = '192.168.1.10'
 port = 8086
 user = [user]
 password = [password]
 dbname= 'water'
-session='pool'
+session='garden'
 
 
 os.system('modprobe w1-gpio')
@@ -146,4 +137,46 @@ while True:
 #print(count_work)
 #print(count_dont)
 #time.sleep(2) 
+
+
+# #!/usr/bin/python
+##encoding:utf-8
+
+#import RPi.GPIO as GPIO                    #Import GPIO library
+#import time                                #Import time library
+#GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering 
+
+#TRIG = 15                                  #Associate pin 15 to TRIG
+#ECHO = 14                                  #Associate pin 14 to Echo
+
+#print "Distance measurement in progress"
+
+#GPIO.setup(TRIG,GPIO.OUT)                  #Set pin as GPIO out
+#GPIO.setup(ECHO,GPIO.IN)                   #Set pin as GPIO in
+
+#while True:
+
+ # GPIO.output(TRIG, False)                 #Set TRIG as LOW
+ # print "Waiting For Sensor To Settle"
+ # time.sleep(2)                            #Delay of 2 seconds
+
+  #GPIO.output(TRIG, True)                  #Set TRIG as HIGH
+  #time.sleep(0.00001)                      #Delay of 0.00001 seconds
+  #GPIO.output(TRIG, False)                 #Set TRIG as LOW
+
+  #while GPIO.input(ECHO)==0:               #Check if Echo is LOW
+   # pulse_start = time.time()              #Time of the last  LOW pulse
+
+  #while GPIO.input(ECHO)==1:               #Check whether Echo is HIGH
+   # pulse_end = time.time()                #Time of the last HIGH pulse 
+
+  #pulse_duration = pulse_end - pulse_start #pulse duration to a variable
+
+  #distance = pulse_duration * 17150        #Calculate distance
+  #distance = round(distance, 2)            #Round to two decimal points
+
+  #if distance > 20 and distance < 400:     #Is distance within range
+   # print "Distance:",distance - 0.5,"cm"  #Distance with calibration
+  #else:
+   # print "Out Of Range"                   #display out of range
 
