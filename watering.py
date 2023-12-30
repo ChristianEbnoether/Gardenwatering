75581b91571e4f53f39237216cdeef829db84e6d
Chris <30667511+ChristianEbnoether@users.noreply.github.com>
2023-12-30 22:26:25 +0100

    Update watering.py
    add new setup stuff

--- watering.py
+++ watering.py
@@ -16,11 +16,6 @@ import requests
 
 GPIO.cleanup()
 
-wattersensor_gpio= 6 #watersensor cable
-relais4_gpio = 5 #Pump
-relais3_gpio = 4 #Ring3
-relais2_gpio = 3 #Ring2
-relais1_gpio = 2 #Ring1
 
 host = '192.168.1.10'
 port = 8086
@@ -30,15 +25,33 @@ dbname= 'water'
 session='garden'
 
 
-GPIO.setmode(GPIO.BCM) 
+watersensor_gpio= 6 #watersensor cable
+relais4_gpio = 5 #Ring3
+relais3_gpio = 4 #Ring2
+relais2_gpio = 3 #Ring1
+relais1_gpio = 1 #Reserve
+relais2_3_gpio = 27 #Trafo
+relais2_4_gpio = 13 #Pump
+
+
+
+GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False) # Ignore warning for now
 
-GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-GPIO.setup(2, GPIO.OUT)
-GPIO.setup(3, GPIO.OUT)
-GPIO.setup(4, GPIO.OUT)
-GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(2, GPIO.OUT) #Relais 1
+GPIO.setup(3, GPIO.OUT) #Relais 2 aka Ring 1
+GPIO.setup(4, GPIO.OUT) #Relais 3 aka Ring 2
+GPIO.setup(5, GPIO.OUT) #Relais 4 aka Ring 3
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 
+GPIO.output(5, GPIO.LOW)
+GPIO.output(4, GPIO.LOW)
+GPIO.output(3, GPIO.LOW)
+GPIO.output(2, GPIO.LOW)
+GPIO.output(27, GPIO.LOW)
+GPIO.output(13, GPIO.LOW)
 
 global wet
 wet=False
@@ -46,17 +59,21 @@ ring1=False
 ring2=False
 ring3=False
 pump=False
-    
+trafo=False
+
 
 def get_sensor_wet():
-    global wet    
+    global wet
     for w in range(1):
         if GPIO.input(6) ==True:
-            print("wassern (6)")
+            #print("wassern (6)")
             wet = False
-        if GPIO.input(6) ==False:
-            print("kein wassern (6)")
+        elif GPIO.input(6) ==False:
+            #print("kein wassern (6)")
             wet = True
+        else:
+            print("else error not /true/not False")
+            pass
 
 def influx():
     global wet
@@ -64,6 +81,7 @@ def influx():
     global ring2
     global ring3
     global pump
+    global trafo
     for t in range(1):
 
         timestamp=datetime.datetime.utcnow().isoformat()
@@ -71,7 +89,7 @@ def influx():
             {
                 "measurement": session,
                 "time": timestamp,
-                "fields": {"wet":wet,"ring1":ring1,"ring2":ring2,"ring3":ring3,"pump":pump}
+                "fields": {"wet":wet,"ring1":ring1,"ring2":ring2,"ring3":ring3,"pump":pump,"trafo":trafo}
                 }
 
             ]
@@ -84,75 +102,102 @@ client = InfluxDBClient(host, port, user, password, dbname)
 
 
 def water_ring1():
+    #Spritzer
     print("Do ring1 now")
-    global wet    
-    global ring1    
-    global pump    
+    global wet
+    global ring1
+    global pump
     if wet==False:
-        GPIO.output(5, GPIO.HIGH) and GPIO.output(2, GPIO.HIGH)
-        r = requests.get("http://[user]:[password]@192.168.97.254/relay/0?turn=on")
-        print('Status Code:')
-        print(r.status_code)
-        ring1=True
+        GPIO.setmode(GPIO.BCM)
+        GPIO.output(2, GPIO.HIGH)
+        GPIO.output(3, GPIO.HIGH)
+        #r = requests.get("http://admin:tsgebch1@192.168.97.254/relay/0?turn=on")
+        #print('Status Code:')
+        #print(r.status_code)
+        ring1 = True
         print(f"wet {wet} Pump {pump} and Ring 1 {ring1} Run")
-        time.sleep(30)
-        GPIO.output(5, GPIO.LOW) and GPIO.output(2, GPIO.LOW)
+        time.sleep(240)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.output(3, GPIO.LOW)
+        time.sleep(2)
+        GPIO.output(2, GPIO.LOW)
         print(f"wet {wet} Pump {pump} and Ring 1 {ring1} not Run")
-        r = requests.get("http://[user]:[password]@192.168.97.254/relay/0?turn=off")
-        print('Status Code:')
-        print(r.status_code)
-        ring1=False
+        #r = requests.get("http://admin:tsgebch1@192.168.97.254/relay/0?turn=off")
+        #print('Status Code:')
+        #print(r.status_code)
+        ring1 = False
+    else:
+        pass
 
 def water_ring2():
+    #Hecke Nord/Nord-Ost
     print("Do ring2 now")
-    global wet    
-    global ring2    
-    global pump    
+    global wet
+    global ring2
+    global pump
     if wet==False:
-        GPIO.output(5, GPIO.HIGH) and GPIO.output(3, GPIO.HIGH)
-        ring2=True
-        r = requests.get("http://[user]:[password]@192.168.97.254/relay/0?turn=on")
-        print('Status Code:')
-        print(r.status_code)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.output(2, GPIO.HIGH)
+        GPIO.output(4, GPIO.HIGH)
+        #r = requests.get("http://admin:tsgebch1@192.168.97.254/relay/0?turn=on")
+        #print('Status Code:')
+        #print(r.status_code)
+        ring2 = True
         print(f"wet {wet} Pump {pump} and Ring 2 {ring2} Run")
-        time.sleep(30)
-        GPIO.output(5, GPIO.LOW) and GPIO.output(3, GPIO.LOW)
-        r = requests.get("http://[user]:[password]@192.168.97.254/relay/0?turn=off")
-        print('Status Code:')
-        print(r.status_code)
-        print(f"wet {wet} Pump {pump} and Ring 2 {ring2} Run")
-        ring2=False
-        
-        
-        
+        time.sleep(240)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.output(4, GPIO.LOW)
+        time.sleep(2)
+        GPIO.output(2, GPIO.LOW)
+        print(f"wet {wet} Pump {pump} and Ring 2 {ring2} not Run")
+        #r = requests.get("http://admin:tsgebch1@192.168.97.254/relay/0?turn=off")
+        #print('Status Code:')
+        #print(r.status_code)
+        ring2 = False
+    else:
+        pass
+
+
 def water_ring3():
+    #Rasen
     print("Do ring3 now")
-    global wet    
-    global ring3    
-    global pump    
+    global wet
+    global ring3
+    global pump
     if wet==False:
-        GPIO.output(5, GPIO.HIGH) and GPIO.output(4, GPIO.HIGH)
-        ring3=True
-        r = requests.get("http://[user]:[password]@192.168.97.254/relay/0?turn=on")
-        print('Status Code:')
-        print(r.status_code)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.output(2, GPIO.HIGH)
+        GPIO.output(5, GPIO.HIGH)
+        #r = requests.get("http://admin:tsgebch1@192.168.97.254/relay/0?turn=on")
+        #print('Status Code:')
+        #print(r.status_code)
+        ring3 = True
         print(f"wet {wet} Pump {pump} and Ring 3 {ring3} Run")
-        time.sleep(30)
-        GPIO.output(5, GPIO.LOW) and GPIO.output(4, GPIO.LOW)
-        r = requests.get("http://[user]:[password]@192.168.97.254/relay/0?turn=off")
-        print('Status Code:')
-        print(r.status_code)
-        print(f"wet {wet} Pump {pump} and Ring 3 {ring3} Run")
-        ring3=False
-        
-   
-schedule.every().day.at("22:58").do(water_ring1)
-schedule.every().day.at("19:55").do(water_ring2)
-schedule.every().day.at("20:15").do(water_ring3)
+        time.sleep(180)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.output(5, GPIO.LOW)
+        time.sleep(2)
+        GPIO.output(2, GPIO.LOW)
+        print(f"wet {wet} Pump {pump} and Ring 3 {ring3} not Run")
+        #r = requests.get("http://admin:tsgebch1@192.168.97.254/relay/0?turn=off")
+        #print('Status Code:')
+        #print(r.status_code)
+        ring3 = False
+    else:
+        pass
+
+print(f"wet {wet} \nPump {pump} \nRing 3 {ring3} \nRing 2 {ring2}  \nRing 1 {ring1} \ntrafo {trafo}")
+
+#schedule.every().day.at("06:30").do(water_ring1)
+#schedule.every().day.at("06:45").do(water_ring2)
+#schedule.every().day.at("06:55").do(water_ring3)
 
 while True:
     get_sensor_wet()
     datapoints=influx()
-    bResult=client.write_points(datapoints)
+    try:
+        client.write_points(datapoints)
+    except Exception as exc:
+        print(exc)
     schedule.run_pending()
     time.sleep(1)
